95b1f76b87e43ccdac96279c065e3ae2d0bcb6a3
Liam Stanley <me@liamstanley.io>
2021-08-14 21:06:53 +0100

    more obvious commit messages

--- sync.sh
+++ sync.sh
@@ -9,5 +9,5 @@ cd "$BASE"
 git config --global user.email "me@liamstanley.io"
 git config --global user.name "Liam Stanley"
 
-git add --all && git commit -m "$(hostname -f)" && git push -u origin master
+git add --all && git commit -m "$(hostname -f): auto-sync" && git push -u origin master
 git pull origin master
